7f1b8d6b0d2279ac9a9a85e65b77857191785341
Steven Robertson <s2robertson@gmail.com>
2023-06-27 22:10:26 -0400

    added dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
 mongoose.connect(connectionString);
